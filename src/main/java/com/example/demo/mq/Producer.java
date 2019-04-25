e624c794db65791d3248259447df524e311d76d3
caoti <2018005126@qq.com>
2019-04-25 09:31:43 +0800

    aaa

--- src/main/java/com/example/demo/mq/Producer.java
+++ src/main/java/com/example/demo/mq/Producer.java
@@ -9,7 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.jms.core.JmsMessagingTemplate;
 import org.springframework.stereotype.Component;
-
+/**
+ * 
+ * 类名: Producer <br/> 
+ * 功能: TODO ADD FUNCTION. <br/> 
+ * 创建时间: 2019年4月25日 上午9:13:00 <br/> 
+ * 
+ * @author caoti 
+ * @version  
+ * @since Jdk 1.8
+ */
 @Component
 public class Producer implements CommandLineRunner{
 	@Autowired
