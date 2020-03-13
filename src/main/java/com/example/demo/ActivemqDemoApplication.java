9a825a5027d8c594125ef933fe3417577c9ee448
caoti <2018005126@qq.com>
2020-03-13 10:44:13 +0800

    删除重复的东西

--- src/main/java/com/example/demo/ActivemqDemoApplication.java
+++ src/main/java/com/example/demo/ActivemqDemoApplication.java
@@ -1,12 +1,23 @@
 package com.example.demo;
 
+import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
 
+import javax.jms.Queue;
+
+@EnableJms
 @SpringBootApplication
 public class ActivemqDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ActivemqDemoApplication.class, args);
+	}
+
+	@Bean
+	public Queue queue(){
+		return new ActiveMQQueue("activemqTest");
 	}
 }
